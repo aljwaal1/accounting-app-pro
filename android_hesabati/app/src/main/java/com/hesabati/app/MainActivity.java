b35b179b7fd956fb8dc3008552174ec5d35bd985
aljwaal1 <yaya15112016@gmail.com>
2026-08-30 19:49:23 +0300

    Fix Android PrintManager import

--- android_hesabati/app/src/main/java/com/hesabati/app/MainActivity.java
+++ android_hesabati/app/src/main/java/com/hesabati/app/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hesabati.app;
 
 import android.app.Activity;
-import android.app.PrintManager;
+import android.print.PrintManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
